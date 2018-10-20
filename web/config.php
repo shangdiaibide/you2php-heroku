640d32c3c8289d626475beb50cb3e708e399593a
shangdiaibide <597223649@qq.com>
2018-10-20 10:11:22 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', '*********************************');//这里改成您的api key
-define('GJ_CODE', 'KR');//国家代码
-define('SITE_NAME', 'Youtube');//网站名字
-define('TITLENAME', 'Youtube');//网站名字
-define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
-define('EMAIL', '123@123.com');//邮箱
+define('APIKEY', 'AIzaSyDhj-AE-_I5JG9oVSpiRCL7-ZcM9Izv6rA');//这里改成您的api key
+define('GJ_CODE', 'TW');//国家代码
+define('SITE_NAME', 'Thinking with God');//网站名字
+define('TITLENAME', 'Thinking With God');//网站名字
+define('EN2DEKEY', 'AnQUP1XDEF48748445435e67CtzDSh4');//一个随机字符串，加解密KEY
+define('EMAIL', 'handsomezhaoyi@gmail.com');//邮箱
 ?>
